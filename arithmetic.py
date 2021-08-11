9fe1d0abb3fa5e4e79a33eaf03427316c53b269f
harshdhiman-ai <harsh.dhiman@aii.ac.in>
2021-08-11 21:32:34 +0530

    commit number 2 on 11th Aug

---
+++ arithmetic.py
@@ -0,0 +1,16 @@
+import numpy as np
+import math
+import cmath
+
+def add(x,y):
+    return print("The addition is",x+y) 
+
+def sub(x,y):
+    return print("The subtraction is",x-y)
+
+
+def sumzz(n):
+    sum=0
+    for i in range(n):
+        sum=sum+i
+    return print("The sum is",sum)
